be04edee68b4c8a3c7b9d42416d3fffa171eeefd
Oscar Velásquez <oscarin0286@gmail.com>
2024-08-31 09:35:31 -0500

    Avances vistas blade pyoyecto

---
+++ resources/views/productos/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('título', "Nuestros productos")
+
+@section('contenido')
+
+  @foreach ($productos as $producto)
+    <div class="card bg-base-100 w-96 shadow-xl">
+      <figure>
+        <img
+          src="https://picsum.photos/id/{{ $producto->id }}/240"
+          alt="{{ $producto->nombre }}" />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">{{ $producto->nombre }}</h2>
+        <p>{{ $producto->descripcion }}</p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  @endforeach
+
+@endsection
